941baa9fd6cd91a118cb7bbb9e39fb234408a4ab
Pedro de Paula (remoto) <pmdpaula@yahoo.com.br>
2022-08-02 20:00:33 -0300

    feat(message): Método create para o message

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { MessagesService } from './messages.service';
+import { Message } from './message.d';
 
 @Controller('messages')
 export class MessagesController {
@@ -12,5 +13,10 @@ export class MessagesController {
   @Get(':id')
   findById(@Param() params) {
     return this.messagesService.findById(Number(params.id));
+  }
+
+  @Post()
+  create(@Body() message: Message) {
+    return this.messagesService.create(message);
   }
 }
